edec168be0733de1e941470d0689cc5ed6e83319
Lucas-Bueno04 <lucasbueno2004@hotmail.com>
2024-12-20 17:23:23 -0300

    adding ecosystem.config.js

---
+++ venegas_api/venegas_api/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "react-frontend",
+        script: "serve",
+        args: "-s build -l 3000",
+        env: {
+          NODE_ENV: "production"
+        },
+        watch: true
+      }
+    ]
+  };
